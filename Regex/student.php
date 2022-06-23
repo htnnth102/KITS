6405c46bf3b76465a844e2a8719c0f05b0e9c658
nghasky <91023004+nghasky@users.noreply.github.com>
2022-06-23 16:33:44 +0700

    update
    update

---
+++ Regex/student.php
@@ -0,0 +1,16 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php 
+	print_r($_POST);
+
+	echo "<br>Xin chao ".$_POST['fullname'];
+	print_r($_GET);
+?>
+</body>
+</html>
